b3973e00257ca229a018d90a2dd81da2c50004ea
Roppii <ng.roche7@gmail.com>
2020-01-11 00:33:39 +0100

    GameManager and PlayerLife added.

---
+++ Assets/Scripts/GenericSingleton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A generic singleton mother class that can be inherited
+/// by other singleton scripts to avoid boilerplate code.
+/// </summary>
+/// <typeparam name="T">T is a parameter of type Component</typeparam>
+
+public class GenericSingleton<T> : MonoBehaviour where T : Component {
+    private static T instance;
+    public static T Instance {
+        get {
+            if (instance == null) {
+                instance = FindObjectOfType<T>();
+                if (instance == null) {
+                    GameObject go = new GameObject {
+                        name = typeof(T).Name
+                    };
+
+                    instance = go.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    public virtual void Awake() {
+        if (instance == null) {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}
